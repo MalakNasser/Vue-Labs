d03dde64ae8b67704022afde71c24fb4fd8f4df3
Malak Nasser <malaknasser26@gmail.com>
2024-05-07 02:48:59 +0300

    Add Day2

--- day2/part2/src/Store.js
+++ day2/part2/src/Store.js
@@ -11,20 +11,6 @@ let store = createStore({
     SET_BOOKS(state, books) {
       state.Books = books;
     },
-    ADD_BOOK(state, book) {
-      state.Books.push(book);
-    },
-    UPDATE_BOOK(state, payload) {
-      const { id, updatedBook } = payload;
-      const index = state.Books.findIndex((book) => book.id === id);
-      updatedBook["id"] = id;
-      if (index !== -1) {
-        state.Books.splice(index, 1, updatedBook);
-      }
-    },
-    DELETE_BOOK(state, id) {
-      state.Books = state.Books.filter((book) => book.id !== id);
-    },
   },
   actions: {
     async fetchBooks({ commit }) {
@@ -33,17 +19,6 @@ let store = createStore({
         commit("SET_BOOKS", response.data);
       } catch (error) {
         console.error("Error fetching books:", error);
-      }
-    },
-    async addBook({ commit }, newBook) {
-      try {
-        const response = await axios.post(
-          "http://localhost:2000/books",
-          newBook
-        );
-        commit("ADD_BOOK", response.data);
-      } catch (error) {
-        console.error("Error adding book:", error);
       }
     },
   },
